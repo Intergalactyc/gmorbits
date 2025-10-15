73ac4e043fd8b591a503972fb34f6ef981979eca
Intergalactyc <22walkerelliott@gmail.com>
2025-10-15 14:48:03 -0500

    Isochrone and NFW added

--- gmorbits/potentials/_isochrone.py
+++ gmorbits/potentials/_isochrone.py
@@ -5,11 +5,14 @@ import numpy as np
 
 class IsochronePotential(Potential):
     def __init__(self, M: float, b: float):
+        self.GM = G * M
         self.b = b
         self.b2 = b * b
 
     def evaluate(self, x):
-        return -G * self.M / (self.b + np.sqrt(np.dot(x, x) + self.b2))
+        return -self.GM / (self.b + np.sqrt(np.dot(x, x) + self.b2))
 
     def gradient(self, x):
-        pass
+        sqrt_r2b2 = np.sqrt(np.dot(x, x) + self.b2)
+        bsr = self.b + sqrt_r2b2
+        return self.GM / (sqrt_r2b2 * bsr * bsr) * x
